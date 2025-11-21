676b9dd0e05ca09dae0c4fe0b76e4db94eae1271
ahmedtamam-dev <ahmed.tamam@g.austincc.edu>
2025-11-21 02:50:23 +0000

    Added HW09 inventory assignment

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,8 @@ import unittest
 import sys
 import os
 
-from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
-suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
+from tests.homework.i_dictionaries_and_sets import test_dictionaries_and_set
+suite = unittest.TestLoader().loadTestsFromModule(test_dictionaries_and_sets)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 
--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,12 @@
-#
+def add_inventory(widgets: dict, widget_name: str, quantity: int):
+    if widget_name in widgets:
+        widgets[widget_name] += quantity
+    else:
+        widgets[widget_name] = quantity
+
+
+def remove_inventory_widget(widgets: dict, widget_name: str):
+    if widget_name in widgets:
+        del widgets[widget_name]
+        return 'Record deleted'
+    return 'Item not found'
